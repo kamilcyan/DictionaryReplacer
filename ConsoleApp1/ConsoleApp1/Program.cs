86dd49a5de68ad31d87190870ff2ec58e705b00f
kamilcyan <kamil.lucjanek@wp.pl>
2020-11-24 19:49:32 +0100

    Creating dictionary

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Dictionary dictionary = new Dictionary();
+
+            dictionary.AddWords("temp", "temporary");
+
+            foreach (KeyValuePair<string, string> kvp in dictionary.words)
+            {
+                Console.WriteLine("{0}, {1}", kvp.Key, kvp.Value);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Dictionary
+    {
+        public Dictionary<string, string> words = new Dictionary<string, string>();
+
+        public Dictionary<string, string> AddWords(string key, string value)
+        {
+            words.Add(key, value);
+            return words;
         }
     }
 }
