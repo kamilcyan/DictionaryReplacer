02c8407c50f8881269a82e8ee06f405719cdf653
kamilcyan <kamil.lucjanek@wp.pl>
2020-11-24 20:08:37 +0100

    WriteIn

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,17 +10,32 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Program prog = new Program();
+
+            prog.WriteIn();
+
+            Console.ReadLine();
+        }
+
+        public void WriteIn()
+        {
             Dictionary dictionary = new Dictionary();
 
             dictionary.AddWords("temp", "temporary");
+            dictionary.AddWords("adj", "adjustment");
+            dictionary.AddWords("go", "gorilla");
+            dictionary.AddWords("name", "John Wick");
+            dictionary.AddWords("op", "open");
+            dictionary.AddWords("plax", "please");
+            dictionary.AddWords("prog", "DictionaryReplacer");
+            dictionary.AddWords("goal", "Work");
+            dictionary.AddWords("res", "resistance");
+            dictionary.AddWords("nud", "send nudes");
 
             foreach (KeyValuePair<string, string> kvp in dictionary.words)
             {
                 Console.WriteLine("{0}, {1}", kvp.Key, kvp.Value);
             }
-
-
-            Console.ReadLine();
         }
     }
 
