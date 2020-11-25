7689c4d0b67404473b57a4d2ac74454902bd2f70
kamilcyan <kamil.lucjanek@wp.pl>
2020-11-25 17:05:22 +0100

    działa wypisywanie wyrazu pomiędzy znakami \$

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -20,35 +20,64 @@ namespace ConsoleApp1
 
         public void WriteIn()
         {
-            Dictionary dictionary = new Dictionary();
+            //Dictionary dictionary = new Dictionary();
 
-            dictionary.AddWords("temp", "temporary");
-            dictionary.AddWords("adj", "adjustment");
-            dictionary.AddWords("go", "gorilla");
-            dictionary.AddWords("name", "John Wick");
-            dictionary.AddWords("op", "open");
-            dictionary.AddWords("plax", "please");
-            dictionary.AddWords("prog", "DictionaryReplacer");
-            dictionary.AddWords("goal", "Work");
-            dictionary.AddWords("res", "resistance");
-            dictionary.AddWords("nud", "send nudes");
+            //dictionary.AddWords("temp", "temporary");
+            //dictionary.AddWords("adj", "adjustment");
+            //dictionary.AddWords("go", "gorilla");
+            //dictionary.AddWords("name", "John Wick");
+            //dictionary.AddWords("op", "open");
+            //dictionary.AddWords("plax", "please");
+            //dictionary.AddWords("prog", "DictionaryReplacer");
+            //dictionary.AddWords("goal", "Work");
+            //dictionary.AddWords("res", "resistance");
+            //dictionary.AddWords("nud", "send nudes");
 
-            foreach (KeyValuePair<string, string> kvp in dictionary.words)
-            {
-                Console.WriteLine("{0}, {1}", kvp.Key, kvp.Value);
-            }
+            //foreach (KeyValuePair<string, string> kvp in dictionary.words)
+            //{
+            //    Console.WriteLine("{0}, {1}", kvp.Key, kvp.Value);
+            //}
         }
 
         public void Translate(/*Dictionary<string, string> dict*/)
         {
             string input = Console.ReadLine();
-            int first = input.IndexOf(@"\$");
-            int second = input.IndexOf(@"\$");
+
+            string searched = @"\$";
+            char sear2 = '\\';
+            string[] source = input.Split('$');
 
 
-            string splitInput = input.Substring(first, second - first+12);
+            for(int i = 1; i < source.Length-1; i++)
+            {
+                if(source[i][source[i].Length-1] == sear2)
+                {
+                    Console.WriteLine(source[i].Substring(0, source[i].Length - 1));
+                }
 
-            Console.WriteLine(splitInput);
+            }
+
+
+
+
+            //int first = input.IndexOf(@"\$");
+            //int second = input.IndexOf(@"\$");
+
+            //int[] found = new int[input.Length];
+
+            //for(int i = 0; i< input.Length; i++)
+            //{
+            //    //found[i] = 0;
+            //    found[i] = input.IndexOf(@"\$");
+
+            //}
+
+            //for(int i = 0; i < input.Length; i++)
+            //{
+            //    Console.WriteLine(input.Substring(found[i] + 2));
+            //}
+
+            //Console.WriteLine(" {0}", input.Substring(found +2));
             //foreach (string word in splitInput)
             //{
             //    Console.WriteLine(word);
